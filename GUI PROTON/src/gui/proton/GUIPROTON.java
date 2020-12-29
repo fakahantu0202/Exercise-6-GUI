d8fa714fe067dd0630e916aa5867ff61435b0d77
MUHAMMAD FAKHRUDDIN BIN ABU ZARIM <60953941+fakahantu0202@users.noreply.github.com>
2020-12-30 00:01:12 +0800

    Add files via upload

---
+++ GUI PROTON/src/gui/proton/GUIPROTON.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.proton;
+
+/**
+ *
+ * @author Muhammad Fakhruddin
+ */
+public class GUIPROTON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
